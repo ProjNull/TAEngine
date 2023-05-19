591d71a53f7f434d2a264312a73999000ef8fc5d
mobilex1122 <mobilex1122.en@gmail.com>
2023-05-19 11:21:55 +0200

    Logo change

--- app/modules/ui.py
+++ app/modules/ui.py
@@ -20,16 +20,27 @@ def printLogo():
                 X$$$$$$I                   '0$$@-           /$$$$$8^            
                ,d$$$$$p                    C@$8l            lM$$$$$x            
                >B$$$$$}                  'm$$W-             `0$$$$$8.           
-               v$$$$$M`                 `0$$$$$$$$$$$$$$$$$$$$$$$$$$[                .`````.  ''     ''     .`;,`.   .'    `.     `.  .`````.       
-               W$$$$$C.                 q$$%akkkkkkkkkkkkkkkkh@$$$$$C.               UC_++~" <*#i   ,p-   +aL+l![CI  XL.  u@z    1o,  Z0++++`         
-              ?$$$$$B(                "q$$@]                  b$$$$$aI              ^W?      fUq1   +b'  jq`        ,8}  ^&Xd   .wv  >#l              
-              C$$$$$#i                m$$B!                   ($$$$$$[              xh'      p-jc.  Qn  ra^         /W   )8Ial  ~M^  tm               
-             ;h$$$$$q               ^k$$&]                    .%$$$$$O             "bt      >b;im' ;M+ i*i         `Ot  'Qr Or  xL   p[               
-             ]8$$$$$)              "q$$%~                      Z$$$$$$:            ]*uuuuI  wz ,p, cp. fO   'YppO  ih`  la` (d. k_  >#zuuui        
-             c$$$$$B.             .o$$&~                       t$$$$$$v            Yu      >%> .Ut:h}  Q/     ^m/  cY.  xJ. "#!?d,  mX              
-             h$$$$$L             ;b$$@-                        +W$$$$$o"           a<      up   (p}a   m{     >a` '#[  .*}   m{bv  >%~                
-       .^:l>{$$$$$$r>l:`.     ';<k$$@1l".                    ^I-o$$$$$8f!".       tZ`     ,d_   l%Oj   nQ     Uz. th`  )a"   /mB!  ud                 
-     h@$$$$$$$$$$$$$$$$$$8} !o$$$$$$$$$$@r                "d@$$$$$$$$$$$$$@(     .WaZZZ0  +d'    k#i   'zWLtxQW] `dj  .pn    i8Z  ,dkZZZv.            
+               v$$$$$M`                 `0$$$$$$$$$$$$$$$$$$$$$$$$$$[              
+               W$$$$$C.                 q$$%akkkkkkkkkkkkkkkkh@$$$$$C.               
+              ?$$$$$B(                "q$$@]                  b$$$$$aI               
+              C$$$$$#i                m$$B!                   ($$$$$$[               
+             ;h$$$$$q               ^k$$&]                    .%$$$$$O               
+             ]8$$$$$)              "q$$%~                      Z$$$$$$:           
+             c$$$$$B.             .o$$&~                       t$$$$$$v            
+             h$$$$$L             ;b$$@-                        +W$$$$$o"             
+       .^:l>{$$$$$$r>l:`.     ';<k$$@1l".                    ^I-o$$$$$8f!".          
+     h@$$$$$$$$$$$$$$$$$$8} !o$$$$$$$$$$@r                "d@$$$$$$$$$$$$$@(         
+     
+           .`````.  ''     ''     .`;,`.   .'    `.     `.  .`````.  
+           UC_++~" <*#i   ,p-   +aL+l![CI  XL.  u@z    1o,  Z0++++`  
+          ^W?      fUq1   +b'  jq`        ,8}  ^&Xd   .wv  >#l       
+          xh'      p-jc.  Qn  ra^         /W   )8Ial  ~M^  tm        
+         "bt      >b;im' ;M+ i*i         `Ot  'Qr Or  xL   p[        
+         ]*uuuuI  wz ,p, cp. fO   'YppO  ih`  la` (d. k_  >#zuuui    
+         Yu      >%> .Ut:h}  Q/     ^m/  cY.  xJ. "#!?d,  mX         
+         a<      up   (p}a   m{     >a` '#[  .*}   m{bv  >%~         
+        tZ`     ,d_   l%Oj   nQ     Uz. th`  )a"   /mB!  ud          
+       .WaZZZ0  +d'    k#i   'zWLtxQW] `dj  .pn    i8Z  ,dkZZZv.     
     """ + "\n"
     )
 
