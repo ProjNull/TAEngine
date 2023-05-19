f2f80a0b53f2f7f3aa959145522389cda1c75aa1
HyScript7 <hyscript7@seznam.cz>
2023-05-19 11:03:42 +0200

    Add launcher

--- app/main.py
+++ app/main.py
@@ -1,4 +1,43 @@
-from modules.game import Game
+import os
 
-g = Game("test_data.json")
-g()
+from modules.game import Game
+from modules.ui import printLogo as ui_logo
+from modules.ui import printUi as ui_selector
+
+
+def find_json_files():
+    json_files = []
+    cwd = os.getcwd()
+    i = 0
+    for root, dirs, files in os.walk(cwd):
+        for file in files:
+            if file.endswith(".taj"):
+                json_files.append(os.path.join(root, file))
+        i += 1
+        if i >= 512:
+            print("Terminated search for taj files, as it went too deep.")
+            break
+    names = []
+    for i in json_files:
+        names.append(i.replace("\\", "/").split("/")[-1])
+    return [json_files, names]
+
+
+GAMES = find_json_files()
+
+while 1:
+    ui_logo()
+    # Main Loop
+    try:
+        game_file = ui_selector(
+            "Please select an adventure file", GAMES[1], clear_screen=True
+        )
+    except KeyboardInterrupt:
+        break
+    except EOFError:
+        exit(0)
+    game = GAMES[0][game_file]
+    # Run game
+    g = Game(game)
+    g()
+print("Goodbye!")
